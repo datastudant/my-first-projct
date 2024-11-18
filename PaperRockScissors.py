a31985851dabf6bf087ea0d8eede58666a8c96fb
w3schools-test <bengamrarida5@gmail.com>
2024-11-18 15:01:37 +0100

    AGAIN

--- PaperRockScissors.py
+++ PaperRockScissors.py
@@ -1,14 +1,19 @@
-import random
+# call random 
+import random 
+# tubel
 options = ("rock","paper","scissors")
-
+# start whille loop
 running = True
 while running:
+  # Game logic here
   player = None
   computer = random.choice(options)
+  # check input
   while player not in options:
     player = input("Enter a choice (rock, paper, scissors): ")
   print(f"Player : {player}")
   print(f"Computer: {computer}")
+  # check winer and loser 
   if player == computer :
     print("It's a tie !")
   elif player == "rock" and computer == "scissors":
@@ -19,5 +24,6 @@ while running:
     print("You win!")
   else:
     print("You lose!")
+  # check if play again or not 
   if not input("Play again? (y/n): ").lower() == "y":
     break
