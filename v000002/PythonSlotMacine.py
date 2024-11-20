af3303e159019ce7319fd339d291b1d3b6516094
w3schools-test <bengamrarida5@gmail.com>
2024-11-20 17:53:58 +0100

    I Changes README FILLS AND COMPLATE MY PythonSlotMacine PROJCT

--- v000002/PythonSlotMacine.py
+++ v000002/PythonSlotMacine.py
@@ -4,10 +4,23 @@ import random
 def spin_row():
   symbols = ["🍒", "🍊", "🍑", "🍓", "🍇"]
   return [random.choice(symbols) for _ in range(3)]
-def print_row():
-  pass
-def get_payout():
-  pass
+def print_row(row):
+  print("*****************")
+  print(" | ".join(row))
+  print("*****************")
+def get_payout(row, bet):
+  if row[0] == row[1] == row[2]:
+    if row[0] == "🍒":
+      return bet * 3
+    elif row[0] == "🍊":
+      return bet * 4
+    elif row[0] == "🍑":
+      return bet * 5
+    elif row[0] == "🍓":
+      return bet * 10
+    elif row[0] == "🍇":
+      return bet * 20
+  return 0
 def main():
   # Initialize the game logic her 
   balance = 100
@@ -30,6 +43,21 @@ def main():
       continue
     balance -= bet
     row = spin_row()
+    #print(spin_row())
+    print("Spinning ...\n")
+    print_row(row)
+    payout = get_payout(row, bet)
+    balance += payout
+    if payout > 0:
+      print(f"You won {payout}!")
+      print(f"Your Balanc new is: {balance:.2f}")
+    else:
+      print("Better luck next time")
+    play_again = input("Do you want to play again? (y/n): ").lower()
+    if play_again != "y":
+      break
+    
+
 
 
 
