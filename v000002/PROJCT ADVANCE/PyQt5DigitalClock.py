d529f20f4b9d14f56a511e1d0dd1dfc10f304721
w3schools-test <bengamrarida5@gmail.com>
2024-11-24 20:47:44 +0100

    finish DIGIT PyQt5DigitalClock PROJCT

--- v000002/PROJCT ADVANCE/PyQt5DigitalClock.py
+++ v000002/PROJCT ADVANCE/PyQt5DigitalClock.py
@@ -1,34 +1,48 @@
-# Python  PyQt5 Digital Clock 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QApplication
 from PyQt5.QtCore import QTimer, QTime, Qt
-
-
+from PyQt5.QtGui import QFont, QFontDatabase
+import sys
 
 class DigitalClock(QWidget):
-  def __init__(self):
-    super().__init__()
-    self.time_label = QLabel("12:00:00", self)
-    self.time = QTimer(self)
-    self.initUI()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.startClock()
 
-  def initUI(self):
-    self.setWindowTitle("Digital Clock")
-    self.setGeometry(600, 400, 300, 100)
-    vbox = QVBoxLayout()
-    vbox.addWidget(self.time_label)
-    self.setLayout(vbox)
+    def initUI(self):
+        # Set up the main window
+        self.setWindowTitle("Digital Clock")
+        self.setGeometry(600, 400, 300, 100)
+        self.setStyleSheet("background-color: black;")
 
+        # Create and configure the time label
+        self.time_label = QLabel(self)
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet("font-size: 150px; color: #26ff00;")
+        font_id = QFontDatabase.addApplicationFont("v000002/PROJCT ADVANCE/DS-DIGIT.TTF")
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font = QFont(font_family, 150)
+        self.time_label.setFont(my_font)
 
-    self.time_label.setAlignment(Qt.AlignCenter)
-    self.time_label.setStyleSheet("font-size : 150px;"
-                                  "font-family : Arial;"
-                                  "color : #26ff00;")
-    self.setStyleSheet("background-color : black;")
+        # Set up the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.time_label)
+        self.setLayout(layout)
 
+    def startClock(self):
+        # Create a timer to update the clock every second
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateTime)
+        self.timer.start(1000)  # Update every second
+        self.updateTime()  # Initialize the time display
 
-if __name__ == "__main__":
-  app = QApplication(sys.argv)
-  clock = DigitalClock()
-  clock.show()
-  sys.exit(app.exec_())
+    def updateTime(self):
+        # Get the current time and update the label
+        current_time = QTime.currentTime()
+        self.time_label.setText(current_time.toString("hh:mm:ss"))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
